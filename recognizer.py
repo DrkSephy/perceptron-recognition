a84519199b727b116d3e4fbb429a83345863b5f4
DrkSephy <dleonar00@citymail.cuny.edu>
2016-04-28 00:16:59 -0400

    Implemented first pass of training algorithm

--- recognizer.py
+++ recognizer.py
@@ -14,3 +14,61 @@ training_data = [
   (array([-1, -1, -1, +1, +1, +1, +1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, +1, -1, -1, -1, +1, -1, -1, +1, -1, -1, -1, +1, -1, -1, -1, +1, +1, +1, -1, -1]), array([-1, -1, -1, -1, -1, +1, -1])),
   (array([+1, +1, +1, -1, -1, +1, +1, -1, +1, -1, -1, +1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, +1, -1, -1, -1, -1, -1, +1, +1, -1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, -1, -1, +1, -1, -1, -1, +1, -1, -1, -1, +1, -1, +1, +1, +1, -1, -1, +1, +1]), array([-1, -1, -1, -1, -1, -1, +1])),
 ]
+
+# Initialize a matrix of weights (7 x 63)
+w = random.rand(7, 63)
+
+# Learning rate alpha
+alpha = 0.2
+
+# Iterations
+iterations = 1000
+
+# Iteration count
+count = 0
+
+# Train over 1000 iterations
+for i in xrange(iterations):
+
+  # Error count for classification
+  error_count = 0
+
+  # Increment the iteration count
+  count += 1
+
+  # Loop over training data
+  for x, expected in training_data:
+
+    # For each output neuron...
+    for j in xrange(0, 7):
+      
+      # Compute input to output neuron
+      result = dot(x, w[j])
+
+      # Compute the output of the jth neuron
+      output = unit_step(result)
+
+      # Compute the error of the jth neuron
+      error = expected[j] - output
+
+      if error != 0:
+        # Increment error count by 1
+        error_count += 1
+
+        # Increment the corresponding weight vector for the jth neuron
+        w[j] += alpha * error * x
+
+  if error_count == 0:
+    print '{} {} {} : {}'.format('Converged in', str(count) + ' iterations', 'with weights', w)
+    break
+
+# Check if we can successfully classify our data
+for x, _ in training_data:
+  for j in xrange(0, 7):
+    result = dot(x, w[j])
+    print '{}: {} -> {}'.format(j, result, unit_step(result))
+  print '---------------------------'
+
+
+
+
