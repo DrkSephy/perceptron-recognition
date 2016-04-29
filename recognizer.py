40baedcc0ec7e2c32d2a2efa609ade8a518015bc
DrkSephy <dleonar00@citymail.cuny.edu>
2016-04-28 21:42:46 -0400

    Fixed implementation of character recognizer

--- recognizer.py
+++ recognizer.py
@@ -1,8 +1,5 @@
-from random import choice
-from numpy import array, dot, random, all, zeros
-
-# Unit step function
-unit_step = lambda x: -1 if x < 0 else 1
+from numpy.random import choice
+from numpy import array, dot, random, zeros, ones, all
 
 # A, B, C, D, E, J, K
 training_data = [
@@ -15,10 +12,20 @@ training_data = [
   (array([+1, +1, +1, -1, -1, +1, +1, -1, +1, -1, -1, +1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, +1, -1, -1, -1, -1, -1, +1, +1, -1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, -1, -1, +1, -1, -1, -1, +1, -1, -1, -1, +1, -1, +1, +1, +1, -1, -1, +1, +1]), array([-1, -1, -1, -1, -1, -1, +1])),
 ]
 
+# Theshold for activation of neuron
+threshold = 0
+
+# Activation function
+unit_step = lambda x: -1 if x < threshold else 1
+
+# Classes
+categories = ['A','B', 'C', 'D', 'E', 'J', 'K']
+
 # Initialize a matrix of weights (7 x 63)
 w = zeros((7, 63))
 
-print w
+# Initialize bias input vector for outputs
+b = ones(7)
 
 # Learning rate alpha
 alpha = 0.2
@@ -27,7 +34,7 @@ alpha = 0.2
 iterations = 1000
 
 # Iteration count
-count = 0
+iteration_count = 0
 
 # Train over 1000 iterations
 for i in xrange(iterations):
@@ -35,17 +42,17 @@ for i in xrange(iterations):
   # Error count for classification
   error_count = 0
 
-  # Increment the iteration count
-  count += 1
+  # Iteration counter
+  iteration_count += 1
 
   # Loop over training data
   for x, expected in training_data:
 
     # For each output neuron...
     for j in xrange(0, 7):
-      
-      # Compute input to output neuron
-      result = dot(x, w[j])
+
+      # Compute the total input for the jth output neuron
+      result = dot(x, w[j]) + b[j]
 
       # Compute the output of the jth neuron
       output = unit_step(result)
@@ -54,23 +61,58 @@ for i in xrange(iterations):
       error = expected[j] - output
 
       if error != 0:
-        # Increment error count by 1
+        # Increment error count
         error_count += 1
 
-        # Increment the corresponding weight vector for the jth neuron
-        w[j] += alpha * error * x
+        # Update the weight vector for the jth output neuron
+        w[j] += alpha * x * error
+
+        # Update the input bias for the jth output neuron
+        b[j] += alpha * error
 
   if error_count == 0:
-    print '{} {} {} : {}'.format('Converged in', str(count) + ' iterations', 'with weights', w)
     break
 
-# # Check if we can successfully classify our data
+# Check if we can successfully classify our data
 # for x, _ in training_data:
 #   for j in xrange(0, 7):
-#     result = dot(x, w[j])
-#     print '{}: {} -> {}'.format(j, result, unit_step(result))
+#     result = dot(x, w[j]) + b[j]
+#     print '{}: {} -> {}: {}'.format(j, result, unit_step(result), categories[j])
 #   print '---------------------------'
 
+def noise(arr, num):
+  indices = choice(range(62), num, replace=False)
+  for index in indices:
+    # Toggle the value
+    if arr[index] == -1:
+      arr[index] = +1
+    else:
+      arr[index] = -1
+  return arr
+
+# Testing
+
+# 5 Missing Pixels 
+testing_data = [
+  (noise(array([+1, +1, -1, -1, +1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, +1, +1, +1, +1, +1, -1, -1, +1, -1, -1, -1, +1, -1, -1, +1, -1, -1, -1, +1, -1, +1, +1, +1, -1, +1, +1, +1]), 5), array([+1, -1, -1, -1, -1, -1, -1])),
+  (noise(array([+1, +1, +1, +1, +1, +1, -1, -1, +1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, -1, +1, +1, +1, +1, +1, -1, -1, +1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, +1, +1, +1, +1, +1, +1, -1]), 5), array([-1, +1, -1, -1, -1, -1, -1])),
+  (noise(array([-1, -1, +1, +1, +1, +1, +1, +1, +1, -1, -1, -1, -1, +1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, +1, -1, -1, +1, +1, +1, +1, -1]), 5), array([-1, -1, +1, -1, -1, -1, -1])),
+  (noise(array([+1, +1, +1, +1, +1, -1, -1, -1, +1, -1, -1, -1, +1, -1, -1, +1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, +1, -1, +1, +1, +1, +1, +1, -1, -1]), 5), array([-1, -1, -1, +1, -1, -1, -1])),
+  (noise(array([+1, +1, +1, +1, +1, +1, +1, -1, +1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, +1, +1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, +1, +1, +1, +1, +1, +1, +1, +1]), 5), array([-1, -1, -1, -1, +1, -1, -1])),
+  (noise(array([-1, -1, -1, +1, +1, +1, +1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, -1, -1, -1, -1, +1, -1, -1, +1, -1, -1, -1, +1, -1, -1, +1, -1, -1, -1, +1, -1, -1, -1, +1, +1, +1, -1, -1]), 5), array([-1, -1, -1, -1, -1, +1, -1])),
+  (noise(array([+1, +1, +1, -1, -1, +1, +1, -1, +1, -1, -1, +1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, +1, -1, -1, -1, -1, -1, +1, +1, -1, -1, -1, -1, -1, +1, -1, +1, -1, -1, -1, -1, +1, -1, -1, +1, -1, -1, -1, +1, -1, -1, -1, +1, -1, +1, +1, +1, -1, -1, +1, +1]), 5), array([-1, -1, -1, -1, -1, -1, +1])),
+]
+
+# Attempt to classify noisy data
+for x, expected in testing_data:
+  predicted = []
+  for j in xrange(0, 7):
+    result = dot(x, w[j]) + b[j]
+    predicted.append(unit_step(result))
+    # print '{}: {} -> {}, Expected: {}'.format(j, result, unit_step(result), _)
+
+  print 'Expected: {}, Predicted: {}, Match: {}'.format(expected, predicted, predicted == expected)
+  print '-------------------'
 
 
 
